6011658ce6ff2c77f3dc190e41a1d35eaee3ff95
M.A.Miqdad Ali Riza <73341274+it20276614@users.noreply.github.com>
2023-06-02 10:19:50 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from "./assets/iPhone-13-Pink.jpg";
+import product2 from "./assets/iPhone-14-Blue-600x600.jpg";
+import product3 from "./assets/Mac-Book-Air-M1-1.jpg";
+import product4 from "./assets/MacBook-Air-M2-1-600x600.jpg";
+import product5 from "./assets/Pixel-6.jpg"
+import product6 from "./assets/S22-Ultra-Burgendy-600x600.jpg"
+import product7 from "./assets/applewatch series7.jpg"
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone 13 Pink",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Iphone 14 Blue",
+        price: 1299.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Macbook Air M1",
+        price: 1499.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Macbook Air M2",
+        price: 1799.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Google Pixel 6",
+        price: 999.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Galaxy S22 Ultra",
+        price: 1799.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Apple Watch Series 7",
+        price: 1799.0,
+        productImage: product7,
+    }
+]
